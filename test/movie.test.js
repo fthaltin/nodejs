390d21e17955108f860a3b7168724ed000a7ec9e
fat19 <fth.altinn@gmail.com>
2019-05-25 14:28:24 +0300

    post Movie and get a movie detail tests added

--- test/movie.test.js
+++ test/movie.test.js
@@ -6,17 +6,17 @@ const server = require('../app');
 
 chai.use(chaiHttp);
 
-let token;
+let token, movieId;
 
 describe('/api/movies test', () => {
     before((done) => {
-        console.log('ilk ben çalışacam');
+        //console.log('ilk ben çalışacam');
         chai.request(server)
-            .post('/api/authenticate')
-            .send({username: 'fatih2', password: '12345'})
+            .post('/authenticate')
+            .send({username: 'test', password: '12345'})
             .end((err, res) => {
                 token = res.body.token
-                console.log(token)
+                //console.log(token)
                 done();
             })
     })
@@ -29,6 +29,48 @@ describe('/api/movies test', () => {
                 .end((err, res) => {
                     res.should.have.status(200);
                     res.body.should.be.a('array');
+                    done();
+                })
+        })
+    })
+
+    describe('/POST Movie', () => {
+        it('it should post a movie', (done) => {
+            const movie = {
+                title:"Usual Suspects",
+                category:"Movies",
+                year:"1995",
+                imdb_score:"9",
+                date:"12.12.2015",
+                director_id:"5cdd7db1f3acb231acc95a56"
+            }
+            chai.request(server)
+                .post('/api/movies/new')
+                .send(movie)
+                .set('x-access-token', token)
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a('Object');
+                    res.body.should.have.property('title');
+                    res.body.should.have.property('category');
+                    res.body.should.have.property('year');
+                    res.body.should.have.property('imdb_score');
+                    res.body.should.have.property('director_id');
+                    movieId = res.body._id;
+                    done();
+                })
+        })
+    })
+
+    describe('/GET/:movie_id', () => {
+        it('It should GET movie details', (done) => {
+            chai.request(server)
+                .get('/api/movies/'+movieId)
+                .set('x-access-token', token)
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a('Object');
+                    res.body.should.have.property('_id').eql(movieId);
                     done();
                 })
         })
